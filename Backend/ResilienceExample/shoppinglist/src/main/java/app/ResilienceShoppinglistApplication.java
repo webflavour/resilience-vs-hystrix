b449591809789e2d875684074a09bfd3ac4d1ad1
binarystate-paau <p.auer@binarystate.com>
2019-11-20 15:15:28 +0100

    no message

--- Backend/ResilienceExample/shoppinglist/src/main/java/app/ResilienceShoppinglistApplication.java
+++ Backend/ResilienceExample/shoppinglist/src/main/java/app/ResilienceShoppinglistApplication.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
